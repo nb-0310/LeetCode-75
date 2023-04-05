1775f8015f8b53efdf7b571ebae68c554768830a
Nirzar Bhatt <nirzarbhatt031093@gmail.com>
2023-04-05 09:37:11 +0530

    Modifications

--- Level 1/Day 5/Best Time to Buy and Sell Stock/index.py
+++ Level 1/Day 5/Best Time to Buy and Sell Stock/index.py
@@ -13,27 +13,3 @@ class Solution:
             j += 1
         
         return maxProfit
-
-    
-# def maxProfit(prices):
-#         i = 0
-#         j = 1
-
-#         while prices[j] < prices[i]:
-#             j += 1
-#             i += 1
-#             if j == len(prices):
-#                  return 0
-
-#         maxProfit = prices[j] - prices[i]
-
-#         while j < len(prices):
-#             if prices[j] - prices[i] > maxProfit:
-#                 maxProfit = prices[j] - prices[i]
-            
-#             j += 1
-
-#         return maxProfit
-
-# prices = [1,1,5,3,6,20]
-# print (maxProfit(prices))
