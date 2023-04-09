281d694a785e3b74f622f0eef558f02c049eee6c
Nirzar Bhatt <nirzarbhatt031093@gmail.com>
2023-04-09 23:39:47 +0530

    Day 10 Wrap up

---
+++ Level 1/Day 10/Climbing Stairs/index.js
@@ -0,0 +1,13 @@
+var climbStairs = function(n) {
+    if (n <= 2) return n
+
+    const arr = new Array(n + 1).fill(0)
+    arr[1] = 1
+    arr[2] = 2
+
+    for (let i = 3; i <= n; i++) arr[i] = arr[i - 1] + arr[i - 2]
+
+    return arr[n]
+};
+
+console.log (climbStairs (45))
